f62bbe6b4de59f3e413be85463215e61772eac8e
nachof948 <nachof948@gmail.com>
2023-12-24 20:21:07 -0300

    Faltan detalles

--- src/components/Tarjeta-clima/Tarjeta.jsx
+++ src/components/Tarjeta-clima/Tarjeta.jsx
@@ -4,15 +4,27 @@ import { Cargando } from '../Cargando/Cargando';
 import imagenCiudad from './Ciudad.jpg'
 const Tarjeta = ({mostrarInformacion, cargandoInformacion, clima, pronostico}) => {
   
+  /* Fecha de la temperatura actual */
   let hoy = new Date()
   let dia = hoy.getDate()
   let mes =  hoy.getMonth() + 1
   let año = hoy.getFullYear()
   let fecha = `${dia}/${mes}/${año}`
-
+  
+  /* Temperatura principal */
   let url = ""
   let urlIcon = ""
   
+  /* Fecha de las temperaturas posteriores */
+  let pronosticoFecha3=''
+  let pronosticoFecha6=''
+  let pronosticoFecha9=''
+
+
+  /* Temperaturas posteriores */
+  let urlIcon3 = ""
+  let urlIcon6 = ""
+  let urlIcon9 = ""
 
   if(cargandoInformacion){
     return <Cargando />
@@ -20,6 +32,16 @@ const Tarjeta = ({mostrarInformacion, cargandoInformacion, clima, pronostico}) =
   if(mostrarInformacion){
     url = `https://openweathermap.org/img/wn/`
     urlIcon = `${url}${clima.weather[0].icon}.png`
+    
+    /* Iconos */
+    urlIcon3 = `${url}${pronostico.list[1].weather[0].icon}.png`
+    urlIcon6 = `${url}${pronostico.list[2].weather[0].icon}.png`
+    urlIcon9 = `${url}${pronostico.list[3].weather[0].icon}.png`
+    
+    /* Fechas */
+    pronosticoFecha3 = pronostico.list[1].dt_txt.substring(8, 10) + '/' + pronostico.list[1].dt_txt.substring(5, 7) + '/' + pronostico.list[1].dt_txt.substring(0, 4) + ' ' +pronostico.list[1].dt_txt.substring(11, 16)
+    pronosticoFecha6 = pronostico.list[2].dt_txt.substring(8, 10) + '/' + pronostico.list[2].dt_txt.substring(5, 7) + '/' + pronostico.list[2].dt_txt.substring(0, 4) + ' ' +pronostico.list[2].dt_txt.substring(11, 16)
+    pronosticoFecha9 = pronostico.list[3].dt_txt.substring(8, 10) + '/' + pronostico.list[3].dt_txt.substring(5, 7) + '/' + pronostico.list[2].dt_txt.substring(0, 4) + ' ' +pronostico.list[3].dt_txt.substring(11, 16)
   }
   return(
     <section>
@@ -34,13 +56,30 @@ const Tarjeta = ({mostrarInformacion, cargandoInformacion, clima, pronostico}) =
               <img className='img' src={imagenCiudad}alt="Ciudad" />
             </div>
             <div className='tarjeta-info'>
-              <div>
+              <div className='mas-detalles'>
                 <p className='sensacion'>Sensación térmica:<span>{(clima.main.feels_like - 273.15).toFixed(1)}°C</span></p>
                 <p className='sensacion'>Humedad: <span>{(clima.main.humidity)}%</span></p>
                 <p className='sensacion'>Presion: <span>{(clima.main.pressure)}hPa</span></p>
                 <p className='sensacion'>Velocidad del Viento:<span>{(clima.wind.speed)}m/s</span></p>
               </div>
               <hr/>
+              <div className='pronosticos'>
+                <div className="pronostico">
+                  <p className='sensacion'>{pronosticoFecha3}hs</p>
+                  <p className='sensacion'><img src={urlIcon3} alt="Icono" />{pronostico.list[1].weather[0].description}</p>
+                  <p className='pron-temp'>{(pronostico.list[1].main.temp - 273.15).toFixed(1)}°C</p>
+                </div>
+                <div className="pronostico">
+                  <p className='sensacion'>{pronosticoFecha6}hs</p>
+                  <p className='sensacion'><img src={urlIcon6} alt="Icono" />{pronostico.list[2].weather[0].description}</p>
+                  <p className='pron-temp'>{(pronostico.list[2].main.temp - 273.15).toFixed(1)}°C</p>
+                </div>
+                <div className="pronostico">
+                  <p className='sensacion'>{pronosticoFecha9}hs</p>
+                  <p className='sensacion'><img src={urlIcon9} alt="Icono" />{pronostico.list[3].weather[0].description}</p>
+                  <p className='pron-temp'>{(pronostico.list[3].main.temp - 273.15).toFixed(1)}°C</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
