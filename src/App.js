1e0642cfe4ab741f405663d1ae3590963cc30c1b
nachof948 <nachof948@gmail.com>
2023-12-24 20:27:51 -0300

    prsentacion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Clima } from './indice';
 import { NavBar } from './components/NavBar/NavBar';
 
-function App() {
+function App() {  
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCargando(false);
+    }, 2000);
+  }, []);
 
   return (
     <div className="App">
-      <NavBar />
-      <Clima/>
+      {cargando ? (
+        <div className='presentacion'>
+          <h1>PronósticoYa!</h1>
+          <div className="container">
+            <div className="cloud front">
+              <span className="left-front"></span>
+              <span className="right-front"></span>
+            </div>
+            <span className="sun sunshine"></span>
+            <span className="sun"></span>
+            <div className="cloud back">
+              <span className="left-back"></span>
+              <span className="right-back"></span>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <NavBar />
+          <Clima />
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
